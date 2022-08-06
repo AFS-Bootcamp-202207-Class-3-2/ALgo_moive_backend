28f18c6d2810e0c94f61e31526630081bb4952fe
ALeri <1146714186@qq.com>
2022-08-06 19:30:55 +0800

    first commit

---
+++ src/main/java/com/algo/c3g2/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.algo.c3g2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String getHello(){
+        return "hello";
+    }
+}
