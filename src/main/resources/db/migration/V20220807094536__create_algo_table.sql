fc9f1739355788237cdc90810817bac000d437ee
YunChen <1183728554@qq.com>
2022-08-07 21:30:17 +0800

    feat:#26 Complete DDL adaptation of PostgreSQL

--- src/main/resources/db/migration/V20220807094536__create_algo_table.sql
+++ src/main/resources/db/migration/V20220807094536__create_algo_table.sql
@@ -1,71 +1,69 @@
+
 create table if not exists t_session(
-    id              varchar(255) primary key comment '场次ID',
-    room_id         varchar(255) not null comment '电影房间ID',
-    movie_id        varchar(255) not null comment '电影ID',
-    seats_info      varchar(1000) not null comment '场次座位分布',
-    start_time      datetime not null  comment '场次开始时间',
-    end_time        datetime not null  comment '场次结束时间',
-    screening_date  date not null  comment '场次日期',
-    price           double not null  comment '场次价格'
+    id              varchar(255) primary key,
+    room_id         varchar(255) not null ,
+    movie_id        varchar(255) not null,
+    seats_info      varchar(1000) not null,
+    start_time      timestamp not null ,
+    end_time        timestamp not null ,
+    screening_date  timestamp not null  ,
+    price           DECIMAL(10,3) not null
 );
 
 create table if not exists t_room(
-   id               varchar(255) primary key comment '观影厅ID',
-   room_name        varchar(255) not null comment '观影厅名',
-   seats_layout     varchar(255) not null comment '观影厅座位',
-   cinema_id        varchar(255) not null comment '电影院ID'
+   id               varchar(255) primary key ,
+   room_name        varchar(255) not null,
+   seats_layout     varchar(255) not null ,
+   cinema_id        varchar(255) not null
 );
 
 create table if not exists t_cinema(
-    id              varchar(255) primary key comment '电影院ID',
-    cinema_name     varchar(255) not null comment '电影院名字',
-    address         varchar(255) default '' comment '电影院地址'
+    id              varchar(255) primary key,
+    cinema_name     varchar(255) not null ,
+    address         varchar(255) default ''
 );
 
 create table if not exists t_movie(
-    id             varchar(255) primary key comment '电影ID',
-    movie_name     varchar(60) not null comment '电影名',
-    actors         varchar(255) default '' comment '演员介绍',
-    movie_desc     varchar(500) default '' comment '电影介绍',
-    release_date   datetime null comment '发布时间',
-    score          float  default 0.0 comment '电影评分',
-    cover          varchar(255) default '' comment '电影封面',
-    video          varchar(255) default '' comment '电影视频',
-    duration       int null comment '持续时间',
-    duration_unit  varchar(255) null comment '持续时间单位'
+    id             varchar(255) primary key,
+    movie_name     varchar(60) not null,
+    actors         varchar(255) default '',
+    movie_desc     varchar(500) default '',
+    release_date   timestamp null,
+    score          float  default 0.0,
+    cover          varchar(255) default '' ,
+    video          varchar(255) default '',
+    duration       float null ,
+    duration_unit  varchar(255) null
 );
 
 CREATE TABLE IF not EXISTS t_user (
-                      id varchar(30) NOT NULL COMMENT 'ID',
-                      username varchar(32) NOT NULL COMMENT '用户名',
-                      nickname varchar(32) NOT NULL COMMENT '用户昵称',
-                      password varchar(60) NOT NULL COMMENT '密码',
-                      role_id varchar(100) DEFAULT '3' COMMENT '角色',
-                      avatar varchar(255) NOT NULL COMMENT '头像地址',
-                      phone varchar(100) DEFAULT '' COMMENT '邮箱地址',
-                      sign varchar(100) DEFAULT '' COMMENT '签名',
-                      status varchar(10) DEFAULT '1' COMMENT '状态：0表示删除，1表示正常',
-                      create_time datetime NULL COMMENT '创建时间',
-                      update_time datetime NULL COMMENT '更新时间',
-                      PRIMARY KEY (`id`)
+                      id varchar(30) primary key,
+                      username varchar(32) not null,
+                      nickname varchar(32) not null,
+                      password varchar(60) not null,
+                      role_id varchar(100) DEFAULT '3',
+                      avatar varchar(255) NOT NULL,
+                      phone varchar(100) DEFAULT '',
+                      sign varchar(100) DEFAULT '',
+                      status varchar(10) DEFAULT '1',
+                      create_time timestamp NULL,
+                      update_time timestamp NULL
 );
 
 CREATE TABLE IF not EXISTS t_role (
-                      id varchar(30) NOT NULL COMMENT 'ID',
-                      role_name varchar(32) NOT NULL COMMENT '角色名称',
-                      PRIMARY KEY (`id`)
+                      id varchar(30) primary key,
+                      role_name varchar(32) NOT NULL
 );
 
 
 CREATE TABLE IF not EXISTS t_order (
-                         id varchar(30) NOT NULL COMMENT 'ID',
-                         price double NOT NULL COMMENT '价钱',
-                         status varchar(10) DEFAULT '0' COMMENT '支付状态：0表示未支付，1表示已支付',
-                         session_id varchar(100) DEFAULT '-1' COMMENT '场次ID',
-                         movie_id varchar(100) DEFAULT '-1' COMMENT '电影ID',
-                         cinema_id varchar(100) DEFAULT '-1' COMMENT '电影院ID',
-                         room_id varchar(100) DEFAULT '-1' COMMENT '电影院ID',
-                         user_id varchar(100) DEFAULT '-1' COMMENT '角色ID',
-                         seat_info varchar(100) DEFAULT '-1' COMMENT '座位信息',
-                         PRIMARY KEY (`id`)
+                         id varchar(30) primary key,
+                         price DECIMAL(10,3) NOT NULL,
+                         status varchar(10) DEFAULT '0',
+                         session_id varchar(100) DEFAULT '-1',
+                         movie_id varchar(100) DEFAULT '-1',
+                         cinema_id varchar(100) DEFAULT '-1',
+                         room_id varchar(100) DEFAULT '-1',
+                         user_id varchar(100) DEFAULT '-1',
+                         seat_info varchar(100) DEFAULT '-1'
 );
