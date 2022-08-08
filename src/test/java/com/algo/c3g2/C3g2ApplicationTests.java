6e1629a302a487faa46bec133eef149e051f67ec
LiuSiAn007 <63287261+LiuSiAn007@users.noreply.github.com>
2022-08-08 11:32:53 +0800

    "fix: ci build fail"

--- src/test/java/com/algo/c3g2/C3g2ApplicationTests.java
+++ src/test/java/com/algo/c3g2/C3g2ApplicationTests.java
@@ -2,8 +2,10 @@ package com.algo.c3g2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class C3g2ApplicationTests {
 
     @Test
