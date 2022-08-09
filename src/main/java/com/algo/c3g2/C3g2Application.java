cdb64485e9ea0c19aa1164c49b8c97371380cd63
LiuSiAn007 <63287261+LiuSiAn007@users.noreply.github.com>
2022-08-09 11:49:36 +0800

    "implement: Open swagger2 whitelist"

--- src/main/java/com/algo/c3g2/C3g2Application.java
+++ src/main/java/com/algo/c3g2/C3g2Application.java
@@ -3,9 +3,11 @@ package com.algo.c3g2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableSwagger2
 public class C3g2Application {
 
     public static void main(String[] args) {
