4372e400d4c95d34f1452d87e3704c3b3238eaa6
LiuSiAn007 <63287261+LiuSiAn007@users.noreply.github.com>
2022-08-09 11:51:03 +0800

    "implement: Open swagger2 whitelist"

--- src/main/java/com/algo/c3g2/Common/config/InterceptorConfig.java
+++ src/main/java/com/algo/c3g2/Common/config/InterceptorConfig.java
@@ -4,6 +4,7 @@ import com.algo.c3g2.interceptor.JwtInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.ArrayList;
@@ -17,14 +18,26 @@ public class InterceptorConfig implements WebMvcConfigurer {
         List<String> patterns = new ArrayList<String>();
         patterns.add("/users/register");
         patterns.add("/login");
-        patterns.add("/movies");
-        patterns.add("/sessions");
-        patterns.add("/cinemas");
+        patterns.add("/movies/**");
+        patterns.add("/sessions/**");
+        patterns.add("/cinemas/**");
+        patterns.add("/swagger-resources/**");
+        patterns.add("/webjars/**");
+        patterns.add("/v2/**");
+        patterns.add("/swagger-ui.html/**");
+
 
         registry.addInterceptor(jwtInterceptor())
                 .addPathPatterns("/**")
                 .excludePathPatterns(patterns);
 
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
 
