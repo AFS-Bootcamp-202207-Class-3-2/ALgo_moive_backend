49c0f4c07b40b06bbb7e016120d61103f7978fb5
Tin <369077336@qq.com>
2022-08-10 13:48:09 +0800

    #44 fix: add field in get movies by cinemaId

--- src/main/java/com/algo/c3g2/repository/MovieRepository.java
+++ src/main/java/com/algo/c3g2/repository/MovieRepository.java
@@ -15,6 +15,6 @@ public interface MovieRepository extends JpaRepository<Movie, String> {
 
     Page<Movie> findAllByMovieNameContainingOrActorsContaining(String movieName, String actor, Pageable pageable);
 
-    @Query(value = "select distinct tm.id,tm.movie_name,tm.actors,tm.movie_desc,tm.release_date,tm.score,tm.cover,tm.video,tm.duration, tm.duration_unit from t_session ts,t_room tr,t_movie tm where tr.id = ts.room_id and tr.cinema_id = ?1 and ts.movie_id = tm.id",nativeQuery = true)
+    @Query(value = "select distinct tm.id,tm.movie_name,tm.actors,tm.movie_desc,tm.release_date,tm.score,tm.cover,tm.video,tm.duration, tm.duration_unit,tm.hot_spot,tm.language_version,tm.movie_type,tm.box_office from t_session ts,t_room tr,t_movie tm where tr.id = ts.room_id and tr.cinema_id = ?1 and ts.movie_id = tm.id",nativeQuery = true)
     List<Movie> findMoviesByCinemaId(String cinemaId);
 }
