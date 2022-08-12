dcf796e68ef0d1e3e61e0db7039fcc153e078a5a
LiuSiAn007 <63287261+LiuSiAn007@users.noreply.github.com>
2022-08-12 11:50:08 +0800

    #null refactor: replace avater

--- src/main/java/com/algo/c3g2/constants/DefaultConstants.java
+++ src/main/java/com/algo/c3g2/constants/DefaultConstants.java
@@ -1,7 +1,7 @@
 package com.algo.c3g2.constants;
 
 public class DefaultConstants {
-    public final static String DEFAULT_AVATAR = "https://czy-blog-system.oss-cn-shenzhen.aliyuncs.com/2020/11/01/374.gif";
+    public final static String DEFAULT_AVATAR = "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2Ftx27%2F49111103186805.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1662868056&t=c70fcfc3c8c920bda48ed708af81722b";
     public final static String DEFAULT_ROLE_ID = "3";
     public final static String DEFAULT_STATUS = "1";
     public final static String DEFAULT_DURATION_UNIT = "minute";
